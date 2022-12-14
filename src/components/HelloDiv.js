7d1f0eae1b09e803d7b47c65639d2e2a0ae53285
AYENEW WALLE <ayeshambel@gmail.com>
2022-12-13 22:52:43 -0500

    my second commit

---
+++ src/components/HelloDiv.js
@@ -0,0 +1,16 @@
+const HelloDiv = () => {
+    const name = "AYENEW WALLE"
+    return(
+        <div>
+            <h1>Hello! My name is {name}</h1>
+            <ul>
+                <li>I love React </li>
+                <li>component is a piece of the UI </li>
+                <li>React app is a tree of component </li>
+
+            </ul>
+        </div>
+    )
+};
+
+export default HelloDiv;
